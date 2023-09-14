5bc48675a547d9241c78502be64433e2e28826e9
Jacob Lang <jacoblang72@comcast.net>
2023-09-14 02:30:52 -0400

    wip

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,12 +1,24 @@
+import { Circles } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
 const Logout = () => {
     const nav = useNavigate(); 
     window.localStorage.clear(); 
     setTimeout(()=> {
         nav("/login")
-    },200)
+    },400)
     return(
-        <div>Loggin Out...</div>
+        <div style={{ flexDirection: "column", height: "100vh", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Circles
+            height="100"
+            width="100"
+            color="royalblue"
+            ariaLabel="circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+        />
+        <h2>Logging Out</h2>
+    </div>
     )
 }
 export default Logout; 
--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -4,12 +4,24 @@ import Navigate from "react-router-dom";
 import UserComponent from "./UserComponent";
 import Login from "./Login";
 import { useNavigate } from "react-router-dom";
+import { Circles } from "react-loader-spinner";
 
 export default function PrivateRoute () {
     const nav = useNavigate();
     return (
         <div>
-    { window.localStorage.getItem("token") ? <UserComponent /> : "no data, return to login"}
+    { window.localStorage.getItem("token") ? <UserComponent /> :  <div style={{ flexDirection: "column", height: "100vh", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <Circles
+                            height="100"
+                            width="100"
+                            color="royalblue"
+                            ariaLabel="circles-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                            visible={true}
+                        />
+                        <h2>Return to Login</h2>
+                    </div>}
     </div>
     )
 }
